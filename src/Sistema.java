692f0bc0d31418e63224ad9a7f610d5e6115c63e
ollivmathe <ollivmathe@gmail.com>
2026-09-02 20:53:19 -0300

    refactor(sistema): adiciona tratamento de excecoes na instanciacao de transacoes

--- src/Sistema.java
+++ src/Sistema.java
@@ -10,20 +10,29 @@ public class Sistema {
 
     public static void main(String[] args) throws Exception {
         List<Transacao> transacoes = new ArrayList<>();
-        Despesa contaDeLuz = new Despesa("Conta de Luz", 150.00, "25/08/2026");
-        Receita salarioMensal = new Receita("Salário Mensal", 5000.00, "25/08/2026");
-        Despesa supermercado = new Despesa("Supermercado", 600.00, "25/08/2026");
+        try {
+            Despesa contaDeLuz = new Despesa("Conta de Luz", 150.00, "25/08/2026");
+            Receita salarioMensal = new Receita("Salário Mensal", 5000.00, "25/08/2026");
+            Despesa supermercado = new Despesa("Supermercado", 600.00, "25/08/2026");
+            Despesa internet = new Despesa("Internet", 99.90, "02/09/2026");
+            //Despesa transacaoInvalida = new Despesa("Multa", -100, "10/02/2026");
 
-        transacoes.add(contaDeLuz);
-        transacoes.add(salarioMensal);
-        transacoes.add(supermercado);
+            transacoes.add(contaDeLuz);
+            transacoes.add(salarioMensal);
+            transacoes.add(supermercado);
+            transacoes.add(internet);
 
-        for (Transacao transacao : transacoes) {
+            for (Transacao transacao : transacoes) {
             transacao.exibirDetalhes();
             if (transacao instanceof Tributavel) {
                 System.out.printf("Imposto Calculado: %.2f\n", ((Tributavel) transacao).calcularImposto());
             }
+
             System.out.println();
+        }
+
+        } catch (Exception e) {
+            System.out.printf("Erro: %s\n", e.getMessage());
         }
     }
 }
